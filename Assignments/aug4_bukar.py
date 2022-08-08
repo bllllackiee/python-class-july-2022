292ae44a31b86a47e59ddc2be8064c1f6ed384f5
bllllackiee <bukarusman2009@gmail.com>
2022-08-08 09:35:29 +0100

    v

--- Assignments/aug4_bukar.py
+++ Assignments/aug4_bukar.py
@@ -16,3 +16,9 @@ print("{}: ".format(fruits[4]["place of birth"]))
 print("{}: ".format(fruits[5]["work experince"]))
 print("{}: ".format(fruits[6]["school attended"]))
 
+me= {'name':'Uthman muhammad ', 'address':'Unguwan sarki', 
+        'number':'09013042149', 'age':13, 'dob':2009, 'pob':'Abuja',
+            'work experience':'1 month', 'schools attented':'ilc schools'}
+
+print('1. Name: {}\n2. Address: {}\n3. Phone Number: {}\n4. Age: {}\n5. Date of Birth: {}\n6. Place of Birth: {}\n7. Work Experience: {}\n8. Schools Attended: {}'.format(me['name'],me['address'],me['number'],me['age'],me['dob'],me['pob'],me['work experience'],me['schools attented']))
+
